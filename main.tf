bd0e9bf78ab05fee3000876d4151f8cff74b7a2d
mrsid743 <siddhantpanekar39@gmail.com>
2025-09-23 22:32:46 +0530

    fix: Use existing security group for Terraform

--- main.tf
+++ main.tf
@@ -5,20 +5,32 @@ provider "aws" {
 }
 
 # --- CHANGE START ---
-# This block is now a "data" source instead of a "resource".
-# It looks up the existing security group by its name instead of creating a new one.
-data "aws_security_group" "strapi_sg" {
-  name = "strapi-security-group"
+# This data source dynamically finds the most recent Amazon Linux 2 AMI
+# in the specified region. This is more reliable than hardcoding an AMI ID.
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
 }
 # --- CHANGE END ---
 
+# This data source looks up the existing security group by its name.
+data "aws_security_group" "strapi_sg" {
+  name = "strapi-security-group"
+}
+
 # Creates an EC2 instance to run the Strapi container
 resource "aws_instance" "strapi_server" {
-  ami           = "ami-02eb7a4783e1ae545" # Amazon Linux 2 AMI for ap-south-1 (Mumbai)
+  # This now refers to the ID of the AMI we looked up dynamically.
+  ami           = data.aws_ami.amazon_linux.id
   instance_type = var.instance_type
   key_name      = var.ec2_key_name # Make sure you have this key pair in your AWS account
   
-  # This now refers to the ID of the data source we looked up.
+  # This refers to the ID of the data source we looked up.
   vpc_security_group_ids = [data.aws_security_group.strapi_sg.id]
 
   user_data = templatefile("user-data.sh", {
