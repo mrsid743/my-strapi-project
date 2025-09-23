e7d0c4d7c7ae9955c112768d6bbf5698ff5e9c22
mrsid743 <siddhantpanekar39@gmail.com>
2025-09-23 22:23:44 +0530

    fix: Use existing security group for Terraform

--- main.tf
+++ main.tf
@@ -4,50 +4,23 @@ provider "aws" {
   region = var.aws_region
 }
 
-# Creates a security group to allow HTTP and SSH traffic
-resource "aws_security_group" "strapi_sg" {
-  name        = "strapi-security-group"
-  description = "Allow HTTP and SSH inbound traffic"
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 1337
-    to_port     = 1337
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "strapi-sg"
-  }
+# --- CHANGE START ---
+# This block is now a "data" source instead of a "resource".
+# It looks up the existing security group by its name instead of creating a new one.
+data "aws_security_group" "strapi_sg" {
+  name = "strapi-security-group"
 }
+# --- CHANGE END ---
 
 # Creates an EC2 instance to run the Strapi container
 resource "aws_instance" "strapi_server" {
   ami           = "ami-02eb7a4783e1ae545" # Amazon Linux 2 AMI for ap-south-1 (Mumbai)
   instance_type = var.instance_type
   key_name      = var.ec2_key_name # Make sure you have this key pair in your AWS account
-  vpc_security_group_ids = [aws_security_group.strapi_sg.id]
+  
+  # This now refers to the ID of the data source we looked up.
+  vpc_security_group_ids = [data.aws_security_group.strapi_sg.id]
+
   user_data = templatefile("user-data.sh", {
     strapi_image = var.strapi_image_tag,
     aws_region   = var.aws_region
