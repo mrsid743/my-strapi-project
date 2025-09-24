11796d4c8ab11e026bbe59d8714cf5ad9b405cc5
mrsid743 <siddhantpanekar39@gmail.com>
2025-09-24 19:18:49 +0530

    Update EC2 key pair name to strapi-mumbai-key

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,10 +1,14 @@
-output "instance_public_ip" {
-  description = "The public IP address of the EC2 instance."
+output "ecr_repository_url" {
+  description = "The URL of the ECR repository."
+  value       = aws_ecr_repository.strapi_ecr_repo.repository_url
+}
+
+output "ec2_public_ip" {
+  description = "The public IP address of the Strapi EC2 instance."
   value       = aws_instance.strapi_server.public_ip
 }
 
-output "instance_public_dns" {
-  description = "The public DNS of the EC2 instance."
-  value       = aws_instance.strapi_server.public_dns
+output "ssh_command" {
+  description = "Command to SSH into the EC2 instance."
+  value       = "ssh -i \"~/.ssh/${var.aws_key_pair_name}.pem\" ec2-user@${aws_instance.strapi_server.public_ip}"
 }
-
