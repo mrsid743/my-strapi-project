d8db88e228203b7230acc4544908fb05948ea547
mrsid743 <siddhantpanekar39@gmail.com>
2025-09-24 07:04:38 +0530

    fix: Remove duplicate outputs.tf file

--- outputs.tf
+++
@@ -1,6 +0,0 @@
-# outputs.tf - Defines the output values from our Terraform configuration.
-
-output "instance_public_ip" {
-  description = "Public IP address of the EC2 instance."
-  value       = aws_instance.strapi_server.public_ip
-}
