3a8965f396d6546ba57fcc2028934a2e0b6f4632
mrsid743 <siddhantpanekar39@gmail.com>
2025-09-24 06:58:15 +0530

    fix: Use existing security group for Terraform

--- main.tf
+++ main.tf
@@ -4,7 +4,6 @@ provider "aws" {
   region = var.aws_region
 }
 
-# --- CHANGE START ---
 # This data source dynamically finds the most recent Amazon Linux 2 AMI
 # in the specified region. This is more reliable than hardcoding an AMI ID.
 data "aws_ami" "amazon_linux" {
@@ -16,22 +15,33 @@ data "aws_ami" "amazon_linux" {
     values = ["amzn2-ami-hvm-*-x86_64-gp2"]
   }
 }
-# --- CHANGE END ---
 
 # This data source looks up the existing security group by its name.
 data "aws_security_group" "strapi_sg" {
   name = "strapi-security-group"
 }
 
+# --- THIS SECTION WAS MISSING ---
+# Creates an IAM instance profile to attach the role to the EC2 instance.
+# The role "EC2-ECR-Pull-Role" must be created in the AWS Console first.
+resource "aws_iam_instance_profile" "ecr_profile" {
+  name = "EC2-ECR-Pull-Profile"
+  role = "EC2-ECR-Pull-Role"
+}
+# --------------------------------
+
 # Creates an EC2 instance to run the Strapi container
 resource "aws_instance" "strapi_server" {
-  # This now refers to the ID of the AMI we looked up dynamically.
   ami           = data.aws_ami.amazon_linux.id
   instance_type = var.instance_type
-  key_name      = var.ec2_key_name # Make sure you have this key pair in your AWS account
-  
-  # This refers to the ID of the data source we looked up.
+  key_name      = var.ec2_key_name
+
   vpc_security_group_ids = [data.aws_security_group.strapi_sg.id]
+
+  # --- THIS LINE WAS MISSING ---
+  # This attaches the IAM role to the EC2 instance, granting it permissions.
+  iam_instance_profile = aws_iam_instance_profile.ecr_profile.name
+  # -----------------------------
 
   user_data = templatefile("user-data.sh", {
     strapi_image = var.strapi_image_tag,
@@ -41,5 +51,12 @@ resource "aws_instance" "strapi_server" {
   tags = {
     Name = "StrapiInstance"
   }
+}
+
+# This output block will display the public IP address of the EC2 instance
+# after terraform apply is complete.
+output "instance_public_ip" {
+  description = "The public IP address of the EC2 instance."
+  value       = aws_instance.strapi_server.public_ip
 }
 
