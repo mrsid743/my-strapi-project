8e97ee1a09c855f9f990c64dfbce2b39b1470741
mrsid743 <siddhantpanekar39@gmail.com>
2025-09-25 10:56:07 +0530

    feat: Sync all Terraform and workflow files

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,157 +1,74 @@
 #####################################
-# Provider
+# General Variables
 #####################################
-provider "aws" {
-  region = var.aws_region
+
+variable "aws_region" {
+  description = "The AWS region to deploy resources in."
+  type        = string
+  default     = "ap-south-1"
+}
+
+variable "ecr_repository_name" {
+  description = "The name of the ECR repository."
+  type        = string
+  default     = "siddhant-strapi"
+}
+
+variable "aws_key_pair_name" {
+  description = "The name of the EC2 key pair to use for SSH access."
+  type        = string
+  default     = "strapi-mumbai-key"
+}
+
+variable "ec2_instance_type" {
+  description = "The instance type for the EC2 server."
+  type        = string
+  default     = "t2.micro"
+}
+
+variable "image_tag" {
+  description = "The Docker image tag to deploy. This is passed from the CI workflow."
+  type        = string
 }
 
 #####################################
-# Data Sources
+# IAM Instance Profile
 #####################################
-data "aws_ami" "amazon_linux_2" {
-  most_recent = true
-  owners      = ["amazon"]
 
-  filter {
-    name   = "name"
-    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
-  }
-}
-
-data "aws_vpc" "default" {
-  default = true
-}
-
-data "aws_subnets" "default_public" {
-  filter {
-    name   = "vpc-id"
-    values = [data.aws_vpc.default.id]
-  }
-
-  filter {
-    name   = "map-public-ip-on-launch"
-    values = ["true"]
-  }
+variable "existing_iam_instance_profile_name" {
+  description = "Optional: Use an existing IAM Instance Profile for the EC2 instance instead of creating a new one. Leave empty to use Terraform-created profile."
+  type        = string
+  default     = ""
 }
 
 #####################################
-# ECR Repository
+# Strapi Secrets
 #####################################
-resource "aws_ecr_repository" "strapi_ecr_repo" {
-  name                 = var.ecr_repository_name
-  image_tag_mutability = "MUTABLE"
 
-  image_scanning_configuration {
-    scan_on_push = true
-  }
-
-  tags = {
-    Name    = "${var.ecr_repository_name}-repo"
-    Project = "StrapiDeployment"
-  }
+variable "strapi_app_keys" {
+  description = "Comma-separated list of secret keys for Strapi."
+  type        = string
+  sensitive   = true
+  default     = "changeThisKey1,andThisKey2AsWell"
 }
 
-#####################################
-# IAM Role & Policies
-#####################################
-resource "aws_iam_role" "ec2_ecr_full_access_role" {
-  name = "ec2_ecr_full_access_role"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [{
-      Effect    = "Allow",
-      Principal = { Service = "ec2.amazonaws.com" },
-      Action    = "sts:AssumeRole"
-    }]
-  })
+variable "strapi_api_token_salt" {
+  description = "Salt for API tokens."
+  type        = string
+  sensitive   = true
+  default     = "changeThisSalt"
 }
 
-resource "aws_iam_role_policy_attachment" "ecr_full" {
-  role       = aws_iam_role.ec2_ecr_full_access_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryFullAccess"
+variable "strapi_admin_jwt_secret" {
+  description = "JWT secret for the admin panel."
+  type        = string
+  sensitive   = true
+  default     = "changeThisAdminSecret"
 }
 
-resource "aws_iam_role_policy_attachment" "ec2_full" {
-  role       = aws_iam_role.ec2_ecr_full_access_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2FullAccess"
-}
-
-resource "aws_iam_role_policy_attachment" "ssm_core" {
-  role       = aws_iam_role.ec2_ecr_full_access_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
-}
-
-resource "aws_iam_instance_profile" "strapi_ec2_profile" {
-  name = "ec2_ecr_full_access_role_profile"
-  role = aws_iam_role.ec2_ecr_full_access_role.name
-}
-
-#####################################
-# Security Group
-#####################################
-resource "aws_security_group" "strapi_sg" {
-  name        = "strapi-sg"
-  description = "Allow inbound HTTP and Strapi traffic"
-  vpc_id      = data.aws_vpc.default.id
-
-  ingress {
-    description = "Allow HTTP"
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    description = "Allow Strapi"
-    from_port   = 1337
-    to_port     = 1337
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    description = "Allow all outbound traffic"
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "strapi-security-group"
-  }
-}
-
-#####################################
-# EC2 Instance
-#####################################
-resource "aws_instance" "strapi_server" {
-  ami                         = data.aws_ami.amazon_linux_2.id
-  instance_type               = var.ec2_instance_type
-  key_name                    = var.aws_key_pair_name
-  vpc_security_group_ids      = [aws_security_group.strapi_sg.id]
-  subnet_id                   = element(data.aws_subnets.default_public.ids, 0)
-  associate_public_ip_address = true
-
-  iam_instance_profile = var.existing_iam_instance_profile_name != "" ? var.existing_iam_instance_profile_name : aws_iam_instance_profile.strapi_ec2_profile.name
-
-  user_data = templatefile("${path.module}/user_data.tpl", {
-    aws_region             = var.aws_region
-    ecr_repo_url           = aws_ecr_repository.strapi_ecr_repo.repository_url
-    image_tag              = var.image_tag
-    strapi_app_keys        = var.strapi_app_keys
-    strapi_api_token_salt  = var.strapi_api_token_salt
-    strapi_admin_jwt_secret= var.strapi_admin_jwt_secret
-    strapi_jwt_secret      = var.strapi_jwt_secret
-  })
-
-  tags = {
-    Name = "Strapi-EC2-Server"
-  }
-
-  lifecycle {
-    create_before_destroy = true
-  }
+variable "strapi_jwt_secret" {
+  description = "JWT secret for API users."
+  type        = string
+  sensitive   = true
+  default     = "changeThisJwtSecret"
 }
