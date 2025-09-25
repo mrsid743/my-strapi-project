305f6b4bc9706e025018c7b1ad9ca455d8447249
mrsid743 <siddhantpanekar39@gmail.com>
2025-09-25 10:43:56 +0530

    feat: Sync all Terraform and workflow files

--- terraform/main.tf
+++ terraform/main.tf
@@ -155,16 +155,3 @@ resource "aws_instance" "strapi_server" {
     create_before_destroy = true
   }
 }
-
-#####################################
-# Outputs
-#####################################
-output "ecr_repository_url" {
-  description = "The URL of the ECR repository"
-  value       = aws_ecr_repository.strapi_ecr_repo.repository_url
-}
-
-output "strapi_server_public_ip" {
-  description = "Public IP of the EC2 instance running Strapi"
-  value       = aws_instance.strapi_server.public_ip
-}
